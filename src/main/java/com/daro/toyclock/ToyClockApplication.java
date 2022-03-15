bac5cba7a93f8a03868af6f0b63d4c04cc66d3a8
daro <dudzinski.dar@gmail.com>
2022-03-15 14:35:26 +0100

    Project setup

---
+++ src/main/java/com/daro/toyclock/ToyClockApplication.java
@@ -0,0 +1,13 @@
+package com.daro.toyclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyClockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyClockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daro/toyclock/ToyclockApplicationTests.java
@@ -0,0 +1,15 @@
+package com.daro.toyclock;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyclockApplicationTests {
+
+    @Test
+    @DisplayName("")
+    void test1() {
+    }
+
+}
